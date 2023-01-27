949f572a55b49f10a5da34066565eb6542171738
Geza Czimeth <geza.czimeth@gmail.com>
2023-01-27 18:52:42 +0100

    More unittests have been added

--- src/test/java/com/melita/order/rest/assembler/ProductModelAssemblerTest.java
+++ src/test/java/com/melita/order/rest/assembler/ProductModelAssemblerTest.java
@@ -2,13 +2,16 @@ package com.melita.order.rest.assembler;
 
 import com.melita.order.dto.ProductDTO;
 import com.melita.order.model.Product;
-import com.melita.order.model.ProductName;
 import jakarta.validation.ValidationException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.melita.order.OrderServiceTestConstants.PACKAGE_NAME;
 import static com.melita.order.OrderServiceTestConstants.PRODUCT_NAME;
+import static com.melita.order.model.ProductName.*;
+import static com.melita.order.model.packages.MobilePackage.PostPaid;
+import static com.melita.order.model.packages.TelephonyPackage.FreeOnNet;
+import static com.melita.order.model.packages.TvPackage.TV_90;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -30,6 +33,27 @@ class ProductModelAssemblerTest {
     }
 
     @Test
+    void productDTOAssembledToTelephony() {
+        ProductDTO productDTO = new ProductDTO(Telephony.name(), FreeOnNet.toString());
+        Product actualProduct = productModelAssembler.assemble(productDTO);
+        assertEquals(new Product(Telephony, FreeOnNet), actualProduct);
+    }
+
+    @Test
+    void productDTOAssembledToMobile() {
+        ProductDTO productDTO = new ProductDTO(Mobile.name(), PostPaid.toString());
+        Product actualProduct = productModelAssembler.assemble(productDTO);
+        assertEquals(new Product(Mobile, PostPaid), actualProduct);
+    }
+
+    @Test
+    void productDTOAssembledToTv() {
+        ProductDTO productDTO = new ProductDTO(TV.toString(), TV_90.toString());
+        Product actualProduct = productModelAssembler.assemble(productDTO);
+        assertEquals(new Product(TV, TV_90), actualProduct);
+    }
+
+    @Test
     void productDTOAssembledThrowsExceptionIfPackageNameIsInvalid() {
         ProductDTO productDTO = new ProductDTO(PRODUCT_NAME.toString(), INVALID_PACKAGE);
         assertThrows(ValidationException.class, () -> productModelAssembler.assemble(productDTO));
@@ -43,19 +67,19 @@ class ProductModelAssemblerTest {
 
     @Test
     void productDTOAssembledThrowsExceptionIfProductNameIsInvalidForTelephony() {
-        ProductDTO productDTO = new ProductDTO(ProductName.Telephony.name(), INVALID_PACKAGE);
+        ProductDTO productDTO = new ProductDTO(Telephony.name(), INVALID_PACKAGE);
         assertThrows(ValidationException.class, () -> productModelAssembler.assemble(productDTO));
     }
 
     @Test
     void productDTOAssembledThrowsExceptionIfProductNameIsInvalidForTV() {
-        ProductDTO productDTO = new ProductDTO(ProductName.TV.name(), INVALID_PACKAGE);
+        ProductDTO productDTO = new ProductDTO(TV.name(), INVALID_PACKAGE);
         assertThrows(ValidationException.class, () -> productModelAssembler.assemble(productDTO));
     }
 
     @Test
     void productDTOAssembledThrowsExceptionIfProductNameIsInvalidForMobile() {
-        ProductDTO productDTO = new ProductDTO(ProductName.Mobile.name(), INVALID_PACKAGE);
+        ProductDTO productDTO = new ProductDTO(Mobile.name(), INVALID_PACKAGE);
         assertThrows(ValidationException.class, () -> productModelAssembler.assemble(productDTO));
     }
 }
