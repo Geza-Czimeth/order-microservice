ed08b262c69b69ca09f694ec8a6dd52025d13539
Geza Czimeth <geza.czimeth@gmail.com>
2023-01-27 08:55:31 +0100

    pull request comments addressed

--- src/main/java/com/melita/order/controller/OrderController.java
+++ src/main/java/com/melita/order/controller/OrderController.java
@@ -3,11 +3,12 @@ package com.melita.order.controller;
 import com.melita.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("orders")
@@ -21,6 +22,6 @@ public class OrderController {
 
     @GetMapping("/status")
     public ResponseEntity<String> status() {
-        return new ResponseEntity<String>(statusMessage, HttpStatus.OK);
+        return new ResponseEntity<String>(statusMessage, OK);
     }
 }
--- src/main/java/com/melita/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/melita/order/service/impl/OrderServiceImpl.java
@@ -2,14 +2,12 @@ package com.melita.order.service.impl;
 
 import com.melita.order.entity.OrderDTO;
 import com.melita.order.service.OrderService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @Component
-@AllArgsConstructor
 @Slf4j
 public class OrderServiceImpl implements OrderService {
 
