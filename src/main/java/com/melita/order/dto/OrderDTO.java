38cffadff18ad313249272cdadd9834993bf3efa
Geza Czimeth <geza.czimeth@gmail.com>
2023-01-29 15:34:29 +0100

    Naming of DTO variables changes so Json is not leaking information about data handling

--- src/main/java/com/melita/order/dto/OrderDTO.java
+++ src/main/java/com/melita/order/dto/OrderDTO.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record OrderDTO(@Valid @NotNull CustomerDTO customerDTO, @Valid @NotNull AddressDTO addressDTO,
+public record OrderDTO(@Valid @NotNull CustomerDTO customer, @Valid @NotNull AddressDTO address,
                        @Valid @NotNull InstallationDateTimeDTO installationDateTime,
                        @Valid @NotEmpty @NotNull List<ProductDTO> productList) {
 
